6ecc851fdec5d70d4f9f0213462193ca456a9c23
Diego santana <d-asantana@live.com>
2023-09-10 12:18:46 -0300

    first commit

---
+++ src/test/java/tech/bootcamp/desafio/ada/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bootcamp.desafio.ada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
